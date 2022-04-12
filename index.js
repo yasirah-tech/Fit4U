cdc1565a486cd0160657fee4f2648a03148105a5
emir4ik-code <emirahmetov@gmail.com>
2022-04-12 15:54:01 -0400

    auth part 1

---
+++ index.js
@@ -0,0 +1,32 @@
+//import
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/db");
+const $ = require("jquery")
+const jsdom = require("jsdom")
+
+//start server 
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//endpoints
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use("/auth", require("./routes/jwtAuth"))
+app.use("/home", require("./routes/home"))
+
+//configure port
+const PORT = process.env.PORT || 8000;
+
+//listen to port
+app.listen(PORT, () => {
+    console.log('listening on port 8000');
+});
--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const pool = require("../db")
+const pool = require("../db/db")
 const bcrypt = require("bcrypt")
 const jwtGenerator = require("../util/jwt_generator")
 const validInfo = require("../middleware/validInfo")
