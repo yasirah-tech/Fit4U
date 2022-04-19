ff3da1f0790c4abb1a48aa74764464ff5e62cfc2
Matthew <tanmatt804@gmail.com>
2022-04-18 20:22:58 -0400

    changed prod

--- db_pool.js
+++ db_pool.js
@@ -1,17 +1,18 @@
 const Pool = require("pg").Pool; 
 
 const connectionDevelopment = {
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432, 
-    database: "fit4u"
+  user: "postgres",
+  password: "password",
+  host: "localhost",
+  port: 5432, 
+  database: "fit4u"
+}
+
+
+const connectionProduction = {
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {rejectUnauthorized: false}
 }
 
 const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionDevelopment)
-const connectionProduction = {
-    connectionString: process.env.DATABASE_URL, 
-    ssl: {rejectUnauthorized: false}
-  }
-
 module.exports = pool;
